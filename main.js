4dd7d401bcc6c9c3cdff8922811b0ce79784478c
Davut Dagtekin <dagtekindavut07@gmail.com>
2024-12-21 13:45:20 +0300

    added reverseString, calculator, caesar cipher

--- main.js
+++ main.js
@@ -14,3 +14,47 @@ export function capitalize(word){
     return tempArr.join("");
 }
 
+export function reverseString(word) {
+
+    if (typeof(word) != "string" || word === "" ) {
+        return "Please enter a string";
+    }
+
+    return word.split("").reverse().join("");
+}
+
+export const calculator = {
+    add : (num1, num2)=> num1 + num2,
+    subtract : (num1, num2)=> num1 - num2,
+    divide : (num1, num2)=> num1 / num2,
+    multiply : (num1, num2)=> num1 * num2
+}
+
+export function caesarCipher (word, key) {
+    let tempArr = word.split("");
+    let returnArr = [];
+
+    tempArr.forEach(letter => {
+        if (!/[A-Z]/i.test(letter)){
+            returnArr.push(letter);
+        }
+        else if (letter === letter.toUpperCase()) {
+            if (letter.charCodeAt(0) + key > 90){
+                returnArr.push(String.fromCharCode(letter.charCodeAt(0) + key - 26))
+            }
+            else {
+                returnArr.push(String.fromCharCode(letter.charCodeAt(0) + key))
+            }
+        }
+        else if (letter === letter.toLowerCase()) {
+            if (letter.charCodeAt(0) + key > 122){
+                returnArr.push(String.fromCharCode(letter.charCodeAt(0) + key - 26))
+            }
+            else {
+                returnArr.push(String.fromCharCode(letter.charCodeAt(0) + key))
+            }
+        }
+    });
+
+    return returnArr.join("");
+}
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import { capitalize } from "./main";
+import { capitalize , reverseString , calculator , caesarCipher} from "./main";
 
 
 test('capitalize', () => {
@@ -14,4 +14,50 @@ test('capitalize', () => {
     expect(capitalize("")).toBe("Please enter a string");
     expect(capitalize(5)).toBe("Please enter a string");
     expect(capitalize(true)).toBe("Please enter a string");
+});
+
+test('reverseString', () => {
+
+    // Normal words
+    expect(reverseString("Hello")).toBe("olleH");
+    expect(reverseString("World")).toBe("dlroW");
+    expect(reverseString("Hello World!")).toBe("!dlroW olleH")
+
+    // Single letter
+    expect(reverseString("a")).toBe("a");
+
+    // Wrong inputs
+    expect(reverseString("")).toBe("Please enter a string");
+    expect(reverseString(5)).toBe("Please enter a string");
+    expect(reverseString(true)).toBe("Please enter a string");
+});
+
+test('calculator', ()=> {
+
+    // Add
+    expect(calculator.add(4,6)).toBe(10);
+
+    // Subtract
+    expect(calculator.subtract(8,3)).toBe(5);
+    expect(calculator.subtract(2,7)).toBe(-5);
+
+    // Divide
+    expect(calculator.divide(10,2)).toBe(5);
+    expect(calculator.divide(1,2)).toBe(0.5);
+
+    // Multiply
+    expect(calculator.multiply(6,2)).toBe(12);
+    expect(calculator.multiply(10,0.5)).toBe(5);
+});
+
+test('caesarCipher', ()=> {
+
+    // Normal Text
+    expect(caesarCipher("HELLO WORLD", 1)).toBe("IFMMP XPSME");
+
+    // Normal Text With Specail char
+    expect(caesarCipher("HELLO WORLD!", 1)).toBe("IFMMP XPSME!");
+
+    // Mixed With Specail char and higher key
+    expect(caesarCipher("HeLlO WorLd!", 4)).toBe("LiPpS AsvPh!");
 });
